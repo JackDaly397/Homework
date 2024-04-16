4a8a1632ef577da6db94664b22c54b09f039ed5a
Jack Daly <jackd397@yahoo.com>
2024-04-16 14:47:25 -0400

    script to extract data from nyc open data and stage in azure

--- scripts/main.py
+++ scripts/main.py
@@ -4,8 +4,8 @@ import json
 config_file_path = 'config/config.json'
 
 #load the JSON configuration file
-with open(config_file_path, 'r') as config_file_path
+with open(config_file_path, 'r') as config_file
     config = json.load(config_file)
 
 #print the configuration 
-Connection_STRING = config['CONNECTION_STRING_AZURE_STORAGE']
+ConnectionString = config['connectionString']
